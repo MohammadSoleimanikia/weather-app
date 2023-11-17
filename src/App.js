383eec42100428ed5b0dc6e21a7c714857c90e05
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2023-11-17 21:31:38 +0330

    add dynamic temperature and condition

--- src/App.js
+++ src/App.js
@@ -12,10 +12,28 @@ function App() {
  
   const [cityInput,setCity]=useState("mashhad");
   const [inputText, setInputText] = useState('');
+  const [temper,setTemper]=useState('');
+  const [conditionId,setConditionId]=useState(0);
   const [cityName,setCityName]=useState('');
+  const [wind,setWind]=useState(0);
+  const [humidity,setHumidity]=useState('');
+  const [condition,setCondition]=useState('');
+
+//curent date
+const currentDate = new Date();
+// Define an array of weekday names
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// Define an array of month names
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// Get the day of the week (0-6) and month (0-11) from the date object
+const dayOfWeek = weekdays[currentDate.getDay()];
+const dayOfMonth = currentDate.getDate();
+const month = months[currentDate.getMonth()];
+// Create the formatted date string
+const formattedDate = `${dayOfWeek}, ${dayOfMonth} ${month}`;
 
 
-// using useEffect hook every time the city name changed fetch function called 
+// using useEffect hook every time the city name changed fetch function called ----------------------------------------------------
   useEffect(()=>{async function fetchData() {
     try {
       const response = await fetch(
@@ -23,6 +41,12 @@ function App() {
       );
       const data = await response.json();
       setCityName(data.name);
+      setTemper(data.main.temp);
+      setConditionId(data.weather[0 ].id);
+      setWind(data.wind.speed);
+      setHumidity(data.main.humidity);
+      setCondition(data.weather[0].description)
+      
     } catch (error) {
       console.log('Error:', error);
     }
@@ -48,7 +72,7 @@ if(inputText.trim() !=='')
   return (
     <div className="App">
       <Search handleInputChange={handleInputChange} handleFormSubmit={handleFormSubmit} inputText={inputText}/>
-      <Main city={cityName}/>
+      <Main condition={condition} cityName={cityName} temper={temper} conditionId={conditionId} wind={wind} humidity={humidity} formattedDate={formattedDate}/>
       <Future/>
     </div>
   );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,15 @@
+import rain from '../Assets/rain.png'
 function Main(props){
-    return (<h1>{props.city}</h1>);
+    return (
+        <div className="main">
+            <div className="first-section">
+                <img className='condition-icon' src={rain} alt="condition icon" />
+                <div className='condition-temper'>
+                    <h1>{props.temper}°</h1>
+                    <h2>{props.condition}</h2>
+                </div>
+            </div>
+        </div>
+    )
     }
     export default Main
