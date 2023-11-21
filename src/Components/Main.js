ead64bcaffd8aa686feac81f3031b09fdd8f2853
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2023-11-21 09:27:02 +0330

    add dynamic icon based on condition Id

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,52 @@
-import rain from '../Assets/rain.png'
+
+
 import wind from '../Assets/Wind.png'
 import humidity from '../Assets/Humidity.png'
 
+import snow from '../Assets/snow.png'
+import Storm from '../Assets/Storm.png'
+import rain from '../Assets/rain.png'
+import clear from '../Assets/clear.png'
+import cloud from '../Assets/cloud.png'
+
 function Main(props){
+//set icon source based on condition
+let src;
+const firstDigit = props.conditionId.toString()[0]; // Get the first digit of the number
+switch (firstDigit) {
+  case '2' :
+    
+    src =Storm;
+    break;
+  case '3':
+    src =rain;
+    break;
+case '5':
+    src =rain;
+    break;
+  case '6':
+    src =snow;
+    break;
+  case '7':
+    src =cloud;
+    break;
+case '8':
+    src =cloud;
+    break;
+  default:
+    // Handle other cases
+    src =Storm;
+    break;
+}
+if(props.conditionId==800)
+{src=clear}
+
+
+
     return (
         <div className="main">
             <div className="first-section">
-                <img className='condition-icon' src={rain} alt="condition icon" />
+                <img className='condition-icon' src={src} alt="condition icon" />
                 <div className='condition-temper'>
                     <h1>{Math.round(props.temper)}°</h1>
                     <h2>{props.condition}</h2>
