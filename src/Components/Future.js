05febea3ae7defef88cac55e2cb8026f46b9d9c7
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2023-11-22 17:35:27 +0330

    start adding future items

--- src/Components/Future.js
+++ src/Components/Future.js
@@ -1,6 +1,19 @@
 function Future(){
     return (
-    <h1></h1>
+    <div className="future ">
+        <div className="future-item">
+            <p className='date'>saturday</p>
+            <div className='temper-item'>
+                <p className='condition-item'>Sunny</p>
+                <p className='temper-item'>15/15</p>
+            </div>
+        </div>
+
+        <div className="future-item">
+        </div>
+        <div className="future-item">
+        </div>
+    </div>
     );
     }
     export default Future
